e53d06a471dc664c4dccc350a9aab675b297f549
Rapuyy <56967667+Rapuyy@users.noreply.github.com>
2020-04-03 17:51:07 +0700

    last

--- soal4/soal4b.c
+++ soal4/soal4b.c
@@ -6,55 +6,16 @@
 
 #define MAX 500
 
-int multiply(int x, int res[], int res_size);
-void fac(int n) 
-{ 
-    int res[MAX]; 
-  
-    // Initialize result 
-    res[0] = 1; 
-    int res_size = 1; 
-  
-    // Apply simple factorial formula n! = 1 * 2 * 3 * 4...*n 
-    for (int x=2; x<=n; x++) 
-        res_size = multiply(x, res, res_size); 
-  
-    // cout << "Factorial of given number is \n"; 
-    // printf("\nFactorial of given number is \n");
-    for (int i=res_size-1; i>=0; i--){
-        printf("%d", res[i]);
-    }     
-    printf("\n");    
-} 
-int multiply(int x, int res[], int res_size) 
-{ 
-    int i;
-    int carry = 0;  // Initialize carry 
-  
-    // One by one multiply n with individual digits of res[] 
-    for (i = 0; i < res_size; i++) 
-    { 
-        int prod = res[i] * x + carry; 
-  
-        // Store last digit of 'prod' in res[]   
-        res[i] = prod % 10;   
-  
-        // Put rest in carry 
-        carry  = prod/10;     
-    } 
-  
-    // Put carry in res and increase result size 
-    while (carry) 
-    { 
-        res[res_size] = carry%10; 
-        carry = carry/10; 
-        res_size++; 
-    } 
-    return res_size; 
+int jumlah(int n) {
+    if (n != 0)
+        return n + jumlah(n - 1);
+    else
+        return n;
 }
 
+
 void *faktorial(int angka){
-    fac(angka);
+    printf("%d ", jumlah(angka));
 }
 
 void main()
